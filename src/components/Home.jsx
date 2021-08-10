cf4db3043bb2a117f80b90bef4eea35b9761c87a
maurogoyeneche <goyenechemauro@gmail.com>
2021-08-10 10:13:25 -0300

    Grid Home  & Link en Sidebar(left)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,27 @@
+import Sidebar from "./layouts/Sidebar";
+import Tweetbox from "./tweetBox/TweetBox";
+import TweetList from "./TweetList/TweetList";
+import DiscoverBar from "./layouts/DiscoverBar";
+
 const Home = () => {
-  return "Armar Home";
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3 col-xs-2">
+            <Sidebar />
+          </div>
+          <div className="col-md-6 col-xs-7">
+            <Tweetbox />
+            <TweetList />
+          </div>
+          <div class="col-md-3 col-xs-3">
+            <DiscoverBar />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
--- src/components/layouts/Sidebar.jsx
+++ src/components/layouts/Sidebar.jsx
@@ -1,97 +1,98 @@
+import { Link } from "react-router-dom";
 import styles from "./Sidebar.module.css";
+
 const Sidebar = () => {
   return (
     <div>
-      <div className="col-md-3 col-xs-2  columna-izquierda">
-        <span className="p-3 fs-6 fw-bold hover">
-          <img
-            className={styles.iconoTwitterSidebar}
-            src="../img/logo.png"
-            alt="icono"
-          />
-        </span>
-        <ul className="mt-2 list-goup">
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="/home">
-              <span>
-                <i className="fas fa-home"></i>
-              </span>{" "}
-              Inicio
-            </a>
-          </li>
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="fas fa-hashtag"></i>
-              </span>{" "}
-              Explorar
-            </a>
-          </li>
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="far fa-bell"></i>
-              </span>{" "}
-              Notificaciones
-            </a>
-          </li>
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="far fa-envelope"></i>
-              </span>{" "}
-              Mensajes
-            </a>
-          </li>
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="far fa-bookmark"></i>
-              </span>{" "}
-              Guardados
-            </a>
-          </li>
-          <li className=" list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="far fa-list-alt"></i>
-              </span>{" "}
-              Listas
-            </a>
-          </li>
-          <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="/perfil/<%=user.username%>">
-              <span>
-                <i className="fas fa-user-alt"></i>
-              </span>{" "}
-              Perfil
-            </a>
-          </li>
-          <li className=" list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
-            <a className={styles.aLineColor} href="...">
-              <span>
-                <i className="fas fa-spinner"></i>
-              </span>{" "}
-              Más opciones
-            </a>
-          </li>
-          <div
-            id="btn-plumita"
-            className="
+      <span className="p-3 fs-6 fw-bold hover">
+        <img
+          className={styles.iconoTwitterSidebar}
+          src="../img/logo.png"
+          alt="icono"
+        />
+      </span>
+      <ul className="mt-2 list-goup">
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="/home" className={styles.aLineColor}>
+            <span>
+              <i className="fas fa-home"></i>
+            </span>{" "}
+            Inicio
+          </Link>
+        </li>
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="fas fa-hashtag"></i>
+            </span>{" "}
+            Explorar
+          </Link>
+        </li>
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="far fa-bell"></i>
+            </span>{" "}
+            Notificaciones
+          </Link>
+        </li>
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="far fa-envelope"></i>
+            </span>{" "}
+            Mensajes
+          </Link>
+        </li>
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="far fa-bookmark"></i>
+            </span>{" "}
+            Guardados
+          </Link>
+        </li>
+        <li className=" list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="far fa-list-alt"></i>
+            </span>{" "}
+            Listas
+          </Link>
+        </li>
+        <li className="list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="/perfil/:username" className={styles.aLineColor}>
+            <span>
+              <i className="fas fa-user-alt"></i>
+            </span>{" "}
+            Perfil
+          </Link>
+        </li>
+        <li className=" list-group-item list-group-item-action border-0 p-3 fs-6 fw-bold hover">
+          <Link to="" className={styles.aLineColor}>
+            <span>
+              <i className="fas fa-spinner"></i>
+            </span>{" "}
+            Más opciones
+          </Link>
+        </li>
+        <div
+          id="btn-plumita"
+          className="
         rounded-circle
         text-center
         d-flex
         align-items-center
         justify-content-center
       "
-          >
-            <svg
-              fill="whitesmoke"
-              width="250px"
-              height="25px"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-              className="
+        >
+          <svg
+            fill="whitesmoke"
+            width="250px"
+            height="25px"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            className="
           r-jwli3a
           r-4qtqp9
           r-yyyyoo
@@ -102,14 +103,13 @@ const Sidebar = () => {
           r-lrvibr
           r-lrsllp
         "
-            >
-              <g>
-                <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"></path>
-              </g>
-            </svg>
-          </div>
-        </ul>
-      </div>
+          >
+            <g>
+              <path d="M8.8 7.2H5.6V3.9c0-.4-.3-.8-.8-.8s-.7.4-.7.8v3.3H.8c-.4 0-.8.3-.8.8s.3.8.8.8h3.3v3.3c0 .4.3.8.8.8s.8-.3.8-.8V8.7H9c.4 0 .8-.3.8-.8s-.5-.7-1-.7zm15-4.9v-.1h-.1c-.1 0-9.2 1.2-14.4 11.7-3.8 7.6-3.6 9.9-3.3 9.9.3.1 3.4-6.5 6.7-9.2 5.2-1.1 6.6-3.6 6.6-3.6s-1.5.2-2.1.2c-.8 0-1.4-.2-1.7-.3 1.3-1.2 2.4-1.5 3.5-1.7.9-.2 1.8-.4 3-1.2 2.2-1.6 1.9-5.5 1.8-5.7z"></path>
+            </g>
+          </svg>
+        </div>
+      </ul>
     </div>
   );
 };
