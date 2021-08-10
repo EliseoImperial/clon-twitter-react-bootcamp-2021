cc07cc0994be2d9ce0431237481bce13d01a06b1
EliseoImperial <Eimperial97@outlook.com>
2021-08-10 11:12:45 -0300

    cambios id por clases

--- src/components/layouts/DiscoverBar.jsx
+++ src/components/layouts/DiscoverBar.jsx
@@ -4,80 +4,81 @@ import discoverStyles from "./DiscoverBar.modules.css";
 function DiscoverBar() {
   return (
     <div>
-      <div class="col-md-3">
-        <div class="bg-light">
-          <h5>
-            <strong> A quién seguir</strong>{" "}
-          </h5>
+      <div className="bg-light">
+        <h5>
+          <strong> A quién seguir</strong>{" "}
+        </h5>
 
-          <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
-              <div class="d-flex justify-content-between w-100">
-                <div id={discoverStyles.profilePicture}>
-                  <img
-                    id={discoverStyles.profileRightbar}
-                    class="rounded-circle w-100 m-0 g-0"
-                    src="/img/profile-2.png"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <h5 class="m-0 p-0 g-0">nombre</h5>
-                  <span>@usuario</span>
-                </div>
-                <div>
-                  <button
-                    id=""
-                    class={`btn ${discoverStyles.btnTwitter} rounded-pill`}
-                  >
-                    Siguiendo
-                  </button>
-                </div>
+        <div className="list-group">
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex justify-content-between w-100">
+              <div className={discoverStyles.profilePicture}>
+                <img
+                  className={`${discoverStyles.profileRightbar} rounded-circle w-100 m-0 g-0`}
+                  src="/img/profile-2.png"
+                  alt=""
+                />
               </div>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <div class="d-flex justify-content-between w-100">
-                <div id={discoverStyles.profilePicture}>
-                  <img
-                    id={discoverStyles.profileRightbar}
-                    class="rounded-circle w-100 m-0 g-0"
-                    src="/img/profile-2.png"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <h5 class="m-0 p-0 g-0">nombre</h5>
-                  <span>@usuario</span>
-                </div>
-                <div>
-                  <button id="" class={`btn ${discoverStyles.btnTwitter} rounded-pill`}>
-                    Siguiendo
-                  </button>
-                </div>
+              <div>
+                <h5 className="m-0 p-0 g-0">nombre</h5>
+                <span>@usuario</span>
               </div>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action">
-              <div class="d-flex justify-content-between w-100">
-                <div id={discoverStyles.profilePicture}>
-                  <img
-                    id={discoverStyles.profileRightbar}
-                    class="rounded-circle w-100 m-0 g-0"
-                    src="/img/profile-2.png"
-                    alt=""
-                  />
-                </div>
-                <div>
-                  <h5 class="m-0 p-0 g-0">nombre</h5>
-                  <span>@usuario</span>
-                </div>
-                <div>
-                  <button id="" class={`btn ${discoverStyles.btnTwitter} rounded-pill`}>
-                    Siguiendo
-                  </button>
-                </div>
+              <div>
+                <button
+                  id=""
+                  className={`btn ${discoverStyles.btnTwitter} rounded-pill`}
+                >
+                  Siguiendo
+                </button>
               </div>
-            </a>
-          </div>
+            </div>
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex justify-content-between w-100">
+              <div className={discoverStyles.profilePicture}>
+                <img
+                  className={`${discoverStyles.profileRightbar} rounded-circle w-100 m-0 g-0`}
+                  src="/img/profile-2.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="m-0 p-0 g-0">nombre</h5>
+                <span>@usuario</span>
+              </div>
+              <div>
+                <button
+                  id=""
+                  className={`btn ${discoverStyles.btnTwitter} rounded-pill`}
+                >
+                  Siguiendo
+                </button>
+              </div>
+            </div>
+          </a>
+          <a href="#" className="list-group-item list-group-item-action">
+            <div className="d-flex justify-content-between w-100">
+              <div className={discoverStyles.profilePicture}>
+                <img
+                  className={`${discoverStyles.profileRightbar} rounded-circle w-100 m-0 g-0`}
+                  src="/img/profile-2.png"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h5 className="m-0 p-0 g-0">nombre</h5>
+                <span>@usuario</span>
+              </div>
+              <div>
+                <button
+                  id=""
+                  className={`btn ${discoverStyles.btnTwitter} rounded-pill`}
+                >
+                  Siguiendo
+                </button>
+              </div>
+            </div>
+          </a>
         </div>
       </div>
     </div>
